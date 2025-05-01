79757d33d2d347bb3582f9648e6eb990e8646cda
abs-30 <shakirwazeer10@gmail.com>
2025-05-01 20:18:10 +0530

    Some changes

--- start_conky.sh
+++ start_conky.sh
@@ -3,9 +3,7 @@
 # Start Conky
 conky &
 
-# Wait a moment to ensure Conky is initialized
 sleep 2
 
-# Start your custom script
-/home/shakir/.config/conky/Simple-Conky-theme/start.sh &
+/home/USER_NAME/.config/conky/Simple-Conky-theme/start.sh &
 
